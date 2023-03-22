265c01cc19741dab8d09dc7c62dbb051147e0b2b
celso-rodrigo <celsorodrigoma@gmail.com>
2023-03-22 16:10:21 -0300

    feat: Concluído 5° requisito

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
 def find_duplicate(nums):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or not isinstance(nums, list):
+        return False
+
+    unique_nums = []
+    for num in nums:
+        if not isinstance(num, int) or num < 0:
+            return False
+        elif num in unique_nums:
+            return num
+        else:
+            unique_nums.append(num)
+    return False
