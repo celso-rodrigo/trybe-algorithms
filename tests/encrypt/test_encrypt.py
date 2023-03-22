331a670ee87a82ee324d6dd0a3e1ac9637a74846
celso-rodrigo <celsorodrigoma@gmail.com>
2023-03-22 13:03:19 -0300

    feat: Concluído 2° requisito

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+from pytest import raises
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("Message", 10) == "egasseM"
+    assert encrypt_message("Message", 3) == "seM_egas"
+    assert encrypt_message("Message", 4) == "ega_sseM"
+
+    with raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("Abcdef", None)
+
+    with raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(None, 1)
