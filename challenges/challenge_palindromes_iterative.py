04ecadf1509d89bd625ee4dc9bfa50b7a4570e6a
celso-rodrigo <celsorodrigoma@gmail.com>
2023-03-23 10:58:13 -0300

    feat: Concluído 5° requisito

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,12 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """
+    Retorna True caso palavra seja palíndromo e False caso não
+    """
+    if not word:
+        return False
+
+    for i in range(int(len(word) / 2)):
+        if word[i] != word[len(word) - (1 + i)]:
+            return False
+
+    return True
